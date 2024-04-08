f08b33114da2c75e3a50863f7ad5e7824139e8e5
mahdi-mallaee <mahdi.mallaee@gamil.com>
2024-04-08 13:41:14 +0330

    improve session groupTabs performance

--- actions/session/groupTabs.ts
+++ actions/session/groupTabs.ts
@@ -1,16 +1,21 @@
+import actions from "~actions"
 import type { Tab, TabGroup } from "~utils/types"
 
 const groupTabs = async (groups: TabGroup[], tabs: Tab[], windowTabs: Tab[], windowId: number) => {
-  for (const group of groups) {
-    const tabIds: number[] = []
-    for (const tab of tabs) {
-      if (tab.groupId === group.id) {
-        tabIds.push(windowTabs[tab.index].id)
-      }
+  if (!groups || !groups.length || groups.length === 0 || !actions.window.checkId(windowId)) { return }
+
+  const groupPromises = groups.map(async (group) => {
+    const tabIds: number[] = tabs
+      .filter(tab => tab.groupId === group.id)
+      .map(tab => windowTabs[tab.index].id)
+
+    if (tabIds.length > 0) {
+      const newTabGroupId = await chrome.tabs.group({ tabIds, createProperties: { windowId } })
+      await chrome.tabGroups.update(newTabGroupId, { collapsed: group.collapsed, color: group.color, title: group.title })
     }
-    const groupId = await chrome.tabs.group({ tabIds, createProperties: { windowId } })
-    await chrome.tabGroups.update(groupId, { collapsed: group.collapsed, color: group.color, title: group.title })
-  }
+  })
+
+  await Promise.all(groupPromises)
 }
 
 export default groupTabs
