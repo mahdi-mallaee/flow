9edbae9989df82f6fc49bd309643ca78744ebcfa
mahdi-mallaee <mahdi.mallaee@gmail.com>
2025-06-22 14:55:43 +0330

    Add sticky tab action menu; Add tab item and selction;

--- views/SessionDetailsView/SessionDetailsView.tsx
+++ views/SessionDetailsView/SessionDetailsView.tsx
@@ -1,20 +1,45 @@
 import useSessions from "~hooks/useSessions"
 import './SessionDetailsView.scss'
 import { useParams } from "~node_modules/react-router/dist"
+import { useState } from "react"
+import { MdDelete } from "~node_modules/react-icons/md"
 
 const SessionDetailsView = () => {
   const { id } = useParams()
-  const session = useSessions().find(s => s.id == id)
+  const session = useSessions()?.find(s => s.id == id)
+  const [selectedTabIds, setSelectedTabIds] = useState<number[]>([])
 
   return (
-    <div className="search-view">
+    <div className="session-details-view">
       <div className='view-title session-title '>
         {session?.title}
       </div>
 
-      {session?.tabs?.map(t => {
-        return <div key={t.id || t.index}>{t.title}</div>
-      })}
+      <div className="tabs-container">
+        {session?.tabs?.map(t => {
+          return (
+            <div key={t.id}
+              className={selectedTabIds.includes(t.id) ? "tab selected" : "tab"}
+              onClick={() => {
+                if (selectedTabIds.includes(t.id)) {
+                  setSelectedTabIds(c => c.filter(id => id !== t.id))
+                } else {
+                  setSelectedTabIds(c => [...c, t.id])
+                }
+              }}>
+              <div className="title">{t.title}</div>
+              <div className="url">{t.url}</div>
+            </div>
+          )
+        })}
+      </div>
+
+      {selectedTabIds.length > 0 &&
+        <div className="tab-actions">
+          <div>{selectedTabIds.length} selected</div>
+          <div className="icon-button"><MdDelete /></div>
+        </div>
+      }
 
     </div>
   )
